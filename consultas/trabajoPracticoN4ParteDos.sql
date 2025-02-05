d4979a995de4dc736fb7bd6da67ffcc97b1fca31
Leonardo <leonardosanchez119@gmail.com>
2025-02-05 17:22:03 -0300

    ejercicio 1.6 del tp nr 4 parte 2 realizado

--- consultas/trabajoPracticoN4ParteDos.sql
+++ consultas/trabajoPracticoN4ParteDos.sql
@@ -8,7 +8,7 @@ WHERE id_jefe IN (
     FROM departamento d  
     INNER JOIN ciudad c ON d.id_ciudad = c.id_ciudad  
     INNER JOIN pais p ON c.id_pais = p.id_pais  
-    WHERE p.nombre_pais = 'Argentina'  
+    WHERE p.nombre_pais = 'ARGENTINA'  
     ---validacion para ver si el jefe tiene empleados a cargo
     AND EXISTS (  
         SELECT 1  
@@ -17,4 +17,22 @@ WHERE id_jefe IN (
         AND e.id_distribuidor = d.id_distribuidor  
         AND e.id_jefe = d.jefe_departamento  
     )  
+);
+
+---1.6. Liste el apellido y nombre de los empleados que pertenecen a aquellos
+---departamentos de Argentina y donde el jefe de departamento posee una comisión de más
+---del 10% de la que posee su empleado a cargo.
+
+SELECT e.nombre, e.apellido  
+FROM empleado e  
+INNER JOIN departamento d ON e.id_departamento = d.id_departamento  
+AND e.id_distribuidor = d.id_distribuidor  
+INNER JOIN ciudad c ON d.id_ciudad = c.id_ciudad  
+INNER JOIN pais p ON c.id_pais = p.id_pais  
+WHERE p.nombre_pais = 'ARGENTINA'  
+AND EXISTS (  
+    SELECT 1  
+    FROM empleado e2  
+    WHERE e2.id_jefe = d.jefe_departamento  
+    AND e2.porc_comision > e.porc_comision * 1.1   
 );
