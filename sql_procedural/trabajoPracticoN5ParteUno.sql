9e1f1ea82edad6e2dd5978b25d3f9125ddd04200
Leonardo <leonardosanchez119@gmail.com>
2025-02-10 14:38:07 -0300

    agrego ejercicio 1 inciso a del practico 5 parte 1

--- sql_procedural/trabajoPracticoN5ParteUno.sql
+++ sql_procedural/trabajoPracticoN5ParteUno.sql
@@ -1,1 +1,10 @@
---- practico nro 5 parte 1
+--- practico nro 5 parte 1 inciso a
+
+ALTER TABLE p5p1e1_contiene
+ADD CONSTRAINT FK_P5P1E1_CONTIENE_PALABRA
+FOREIGN KEY (idioma, cod_palabra)
+REFERENCES p5p1e1_palabra (idioma, cod_palabra)
+ON DELETE CASCADE
+
+
+
