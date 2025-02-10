188eaf7e4ef1dc294f569bb71981bdd785a7c8da
Leonardo <leonardosanchez119@gmail.com>
2025-02-10 14:55:48 -0300

    agrego comentario para acordarme que faltan hacer inserts de dos tablas

--- sql_procedural/trabajoPracticoN5ParteUno.sql
+++ sql_procedural/trabajoPracticoN5ParteUno.sql
@@ -7,4 +7,12 @@ REFERENCES p5p1e1_palabra (idioma, cod_palabra)
 ON DELETE CASCADE
 
 
+---inserts de la tabla contiene 
+INSERT INTO p5p1e1_contiene (id_articulo, idioma, cod_palabra)
+VALUES (1, 'ES', 1)
+INSERT INTO p5p1e1_contiene (id_articulo, idioma, cod_palabra)
+VALUES (2, 'ES', 1)
+INSERT INTO p5p1e1_contiene (id_articulo, idioma, cod_palabra)
+VALUES (1, 'ES', 2)
 
+--- faltan los de la tabla palabra y articulo
