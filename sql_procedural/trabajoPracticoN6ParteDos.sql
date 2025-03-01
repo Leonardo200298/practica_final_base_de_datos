64af8b1b0916a21cd1e3ce02cf2d21312d09bf15
Leonardo200298 <leonardosanchez119@gmail.com>
2025-03-01 15:39:05 -0300

    planteo de inciso c del ejercicio 2 del tp nro 6 parte 2

--- sql_procedural/trabajoPracticoN6ParteDos.sql
+++ sql_procedural/trabajoPracticoN6ParteDos.sql
@@ -46,3 +46,24 @@ WHERE id_tarea = '519';
 UPDATE tareas
 SET nombre_tarea = 'sin tarea'
 WHERE id_tarea = '520';
+
+/* Ejercicio 2
+A partir del esquema unc_peliculas, realice procedimientos para:
+c) Completar una tabla denominada MAS_ENTREGADAS con los datos de las 20 películas
+más entregadas en los últimos seis meses desde la ejecución del procedimiento. Esta tabla
+por lo menos debe tener las columnas código_pelicula, nombre, cantidad_de_entregas (en
+caso de coincidir en cantidad de entrega ordenar por código de película).
+d) Generar los datos para una tabla denominada SUELDOS, con los datos de los empleados
+cuyas comisiones superen a la media del departamento en el que trabajan. Esta tabla debe
+tener las columnas id_empleado, apellido, nombre, sueldo, porc_comision.
+e) Cambiar el distribuidor de las entregas sucedidas a partir de una fecha dada, siendo que el
+par de valores de distribuidor viejo y distribuidor nuevo es variable. */
+
+---c) Completar una tabla denominada MAS_ENTREGADAS con los datos de las 20 películas
+---más entregadas en los últimos seis meses desde la ejecución del procedimiento. Esta tabla
+---por lo menos debe tener las columnas código_pelicula, nombre, cantidad_de_entregas (en
+---caso de coincidir en cantidad de entrega ordenar por código de película).
+
+/* crear funcion que devuelva tabla, ya que es mas facil al momento ded ejecucion de la funcion
+saber las 20 peliculas mas entregadas */
+
