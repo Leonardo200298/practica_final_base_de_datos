6f736773605d711468a236b7901545e579decd50
Leonardo200298 <leonardosanchez119@gmail.com>
2025-03-02 16:47:14 -0300

    Inciso c del ejercicio 2 del tp nro 6 parte 2 realizado

--- sql_procedural/trabajoPracticoN6ParteDos.sql
+++ sql_procedural/trabajoPracticoN6ParteDos.sql
@@ -67,3 +67,35 @@ par de valores de distribuidor viejo y distribuidor nuevo es variable. */
 /* crear funcion que devuelva tabla, ya que es mas facil al momento ded ejecucion de la funcion
 saber las 20 peliculas mas entregadas */
 
+/* Consulta para crear cursor */
+
+SELECT *
+FROM pelicula p
+INNER JOIN renglonentregapelicula r
+ON p.codigo_pelicula = r.codigo_pelicula
+ORDER BY r.cantidad, r.codigo_pelicula
+LIMIT 20;
+
+CREATE OR REPLACE FUNCTION MAS_ENTREGADAS()
+RETURNS TABLE(codigo_pelicula NUMERIC, nombre VARCHAR, cantidad_de_entregas BIGINT) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        p.codigo_pelicula, 
+        p.titulo AS nombre, 
+        COUNT(*) AS cantidad_de_entregas
+    FROM pelicula p
+    INNER JOIN renglonentregapelicula re ON p.codigo_pelicula = re.codigo_pelicula
+    INNER JOIN entregapelicula e ON re.nro_entrega = e.nro_entrega
+    WHERE e.fecha_entrega >= CURRENT_DATE - INTERVAL '6 months'
+    GROUP BY p.codigo_pelicula, p.titulo
+    ORDER BY cantidad_de_entregas DESC, p.codigo_pelicula ASC
+    LIMIT 20;
+END;
+$$ LANGUAGE plpgsql;
+
+---d) Generar los datos para una tabla denominada SUELDOS, con los datos de los empleados
+---cuyas comisiones superen a la media del departamento en el que trabajan. Esta tabla debe
+---tener las columnas id_empleado, apellido, nombre, sueldo, porc_comision.
+
+
