da96b728899b8491ec304af3ed796a0bc415e56a
Leonardo <leonardosanchez119@gmail.com>
2025-03-10 16:12:34 -0300

    ejemplo consulta optimizada

--- consultas_avanzadas_optimización/ejemploConsultaOptimizada.sql
+++ consultas_avanzadas_optimización/ejemploConsultaOptimizada.sql
@@ -4,3 +4,7 @@
 SELECT id_empleado, nombre
 FROM empleado
 WHERE fecha_nacimiento > to_date('1/1/1980', 'dd/MM/yyyy');
+
+---con indice mejora la consulta 
+CREATE INDEX I_Empleado_Fecha_Nacimiento ON
+EMPLEADO(fecha_nacimiento DESC);
