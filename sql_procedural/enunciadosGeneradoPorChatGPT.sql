b4433f137c5f72b4139a4db269189b1d7b6d4856
Leonardo200298 <leonardosanchez119@gmail.com>
2025-03-12 11:28:25 -0300

    agrego ultimos ejercicios de stored procedure, uno con cursores

--- sql_procedural/enunciadosGeneradoPorChatGPT.sql
+++ sql_procedural/enunciadosGeneradoPorChatGPT.sql
@@ -8,28 +8,52 @@
 ---p5p1e1_contiene
 ---p5p1e1_palabra
 
-CREATE OR REPLACE FUNCTION RESUMEN_PALABRAS() RETURNS TABLE (codigo_palabra, idioma, descripcion, cantidad_articulos) AS $$
-DECLARE
- var_r record;
- 
+CREATE OR REPLACE FUNCTION RESUMEN_PALABRAS() 
+RETURNS TABLE (codigo_palabra NUMERIC, idioma VARCHAR, descripcion TEXT, cantidad_articulos INT) 
+AS $$
 BEGIN
- FOR var_r IN (
-   SELECT 
-   FROM p5p1e1_contiene c
-   INNER JOIN p5p1e1_palabra p
-   ON c.idioma = p.idioma AND
-   c.cod_palabra = p.cod_palabra
-   
- ) 
- LOOP
- 
- RETURN NEXT;
- END LOOP;
- 
+    RETURN QUERY  
+    SELECT 
+        c.cod_palabra, 
+        p.idioma, 
+        p.descripcion, 
+        COUNT(c.id_articulo) AS cantidad_articulos
+    FROM p5p1e1_contiene c
+    INNER JOIN p5p1e1_palabra p
+        ON c.idioma = p.idioma 
+        AND c.cod_palabra = p.cod_palabra
+    GROUP BY c.cod_palabra, p.idioma, p.descripcion;
 END;
+$$ LANGUAGE plpgsql;
 
-$$ 
-/* Enunciado 2
-Se requiere actualizar la columna nacional de la tabla ARTICULO con el valor 'Nacional' si el autor del artículo es de Argentina y 'Internacional' en caso contrario. Para ello, se dispone de una tabla denominada AUTORES con las columnas nombre_autor y pais. Implementar un procedimiento que realice esta actualización para todos los registros de la tabla ARTICULO.
+---Enunciado 2
+---Se requiere actualizar la columna nacional de la tabla ARTICULO con el valor 'Charles Bukowski' si el autor del artículo es de Argentina y
+--- 'Samid Baradeg' en caso contrario. Para ello, se dispone de una tabla denominada AUTORES con las columnas nombre_autor y pais. Implementar un procedimiento que realice esta ---actualización para todos los registros de la tabla ARTICULO.
 
- */
+CREATE TABLE autores (
+nombre_autor VARCHAR(30),
+pais VARCHAR(30)
+
+)
+
+CREATE OR REPLACE FUNCTION FN_ACTUALIZAR_TABLA_ARTICULO() RETURNS VOID AS $$
+DECLARE
+  var_r record;
+BEGIN
+  FOR var_r IN (
+     SELECT 
+     FROM p5p1e1_articulo a
+     INNER autores au
+     ON a.autor = au.autor
+  )
+  LOOP
+     UPDATE p5p1e1_articulo
+     SET autor = 
+        CASE WHEN autor = 'Charles Bukowski'
+            THEN 'Samid Baradeg'
+            ELSE 'Charles Bukowski'
+        END;
+  END LOOP;
+END;
+$$
+LANGUAGE plpgsql;
